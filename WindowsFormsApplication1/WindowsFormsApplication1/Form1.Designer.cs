472bfb43f4cc8f339eecd4f4aa520e47636b19a0
cdc0012 <cdc0012@uah.edu>
2014-12-07 17:34:13 -0600

    Add NQueens Program to GitHub
    Solves N Queens, finished base product which can be used or updated
    later.

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class NQueensGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNumOfQueens = new System.Windows.Forms.TextBox();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.tbSolutions = new System.Windows.Forms.TextBox();
+            this.tbRunningTime = new System.Windows.Forms.TextBox();
+            this.tbAttempts = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rbGUIOutput = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbNumOfQueens
+            // 
+            this.tbNumOfQueens.Location = new System.Drawing.Point(124, 36);
+            this.tbNumOfQueens.Name = "tbNumOfQueens";
+            this.tbNumOfQueens.Size = new System.Drawing.Size(136, 20);
+            this.tbNumOfQueens.TabIndex = 0;
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Location = new System.Drawing.Point(124, 60);
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.Size = new System.Drawing.Size(136, 20);
+            this.tbStatus.TabIndex = 1;
+            // 
+            // tbSolutions
+            // 
+            this.tbSolutions.Location = new System.Drawing.Point(124, 84);
+            this.tbSolutions.Name = "tbSolutions";
+            this.tbSolutions.Size = new System.Drawing.Size(136, 20);
+            this.tbSolutions.TabIndex = 2;
+            // 
+            // tbRunningTime
+            // 
+            this.tbRunningTime.Location = new System.Drawing.Point(123, 133);
+            this.tbRunningTime.Name = "tbRunningTime";
+            this.tbRunningTime.Size = new System.Drawing.Size(137, 20);
+            this.tbRunningTime.TabIndex = 3;
+            // 
+            // tbAttempts
+            // 
+            this.tbAttempts.Location = new System.Drawing.Point(124, 109);
+            this.tbAttempts.Name = "tbAttempts";
+            this.tbAttempts.Size = new System.Drawing.Size(136, 20);
+            this.tbAttempts.TabIndex = 4;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(13, 43);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(96, 13);
+            this.Label1.TabIndex = 5;
+            this.Label1.Text = "Number of Queens";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(13, 67);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(37, 13);
+            this.Label2.TabIndex = 6;
+            this.Label2.Text = "Status";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(13, 91);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(50, 13);
+            this.Label3.TabIndex = 7;
+            this.Label3.Text = "Solutions";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Running Time";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Attempts";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 105);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Solve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rbGUIOutput
+            // 
+            this.rbGUIOutput.Location = new System.Drawing.Point(299, 36);
+            this.rbGUIOutput.Name = "rbGUIOutput";
+            this.rbGUIOutput.Size = new System.Drawing.Size(277, 498);
+            this.rbGUIOutput.TabIndex = 12;
+            this.rbGUIOutput.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "HH:MM:SS:MS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label7.Location = new System.Drawing.Point(356, 2);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(168, 31);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Solution GUI";
+            // 
+            // NQueensGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 552);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rbGUIOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.tbAttempts);
+            this.Controls.Add(this.tbRunningTime);
+            this.Controls.Add(this.tbSolutions);
+            this.Controls.Add(this.tbStatus);
+            this.Controls.Add(this.tbNumOfQueens);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "NQueensGUI";
+            this.Text = "NQueens Solver by Christopher Cunningham";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNumOfQueens;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.TextBox tbSolutions;
+        private System.Windows.Forms.TextBox tbRunningTime;
+        private System.Windows.Forms.TextBox tbAttempts;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rbGUIOutput;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,288 @@
+﻿/*
+ * NQueens Program written by Christopher Cunningham
+ * This program is for an extra credit project for my Algorithms Class
+ * and Ajinkya Kulkarni
+ * 
+ * This program uses a backtracking algorithm that was not fully written by me, after researching different ways
+ * and looking through different sets of code on GitHub and CodeProject, I zombied something together and learned a lot from it.
+ * 
+ * This program does not run extremely fast, as there are plenty of algorithms that run it way faster, but I am still learning the
+ * process of backtracking this problem. 
+ * 
+ * There is a method named UpdateFields(); that updates the GUI while the program is running, if you comment this call out, it will
+ * decrease the amount of time it takes to find all solutions (~almost %50 decrease in time).
+ * 
+ * There are still a lot of small tweaks I could make to make this program faster, but I feel extremely good about the work that was created
+ * 
+ * */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class NQueensGUI : Form
+    {
+        //initialize global variables
+        TimeSpan tsRunningTime;
+        int iQueens;
+        int iCount = 0;
+        int[,] iChessBoard;
+        int iAttempts;
+        int[] iPlaceHolder;
+
+
+        public NQueensGUI()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //get start time
+            DateTime dtStart = DateTime.Now;
+
+            //clear form data 
+            ClearTextBoxes();
+            ResetVariables();
+            UpdateFields();
+
+            //get user input of queens for NXN use
+            String strQueens = tbNumOfQueens.Text;
+            iQueens = Convert.ToInt32(strQueens);
+
+            
+            //check if user inputted a 0 as this is not a valid size
+            if(iQueens == 0)
+            {
+                rbGUIOutput.AppendText("Enter a number greater than 0.");
+                return;
+            }
+
+
+            //create a new chess board
+            iChessBoard = new int[iQueens, iQueens];
+            iAttempts = 0;
+
+            //Update so user knows what is happening
+            tbStatus.Text = "Calculating..Please Wait.";
+            UpdateFields();
+
+
+            //Create Chess board
+            CreateBoard();
+
+            //take the number of queens so that we can parse through the board
+            iPlaceHolder = new int[iQueens];
+
+            //decrement the placeholder so that we can make sure we have the correct size board
+            for (int i = 0; i < iQueens; i++)
+            {
+                iPlaceHolder[i] = -1;
+            }
+
+
+            //Call BackTracking Algorithm
+            nQueensBackTrack();
+
+
+            //Calculate total running time of calculation
+            DateTime dtEnd = DateTime.Now;
+            tsRunningTime = dtEnd - dtStart;
+            tbRunningTime.Text = Convert.ToString(tsRunningTime);
+        }
+
+
+        public void CreateBoard()
+        {
+            //Create the board and set all [x,y] positions to 0
+            for (int x = 0; x < iQueens; x++)
+            {
+                for (int y = 0; y < iQueens; y++)
+                {
+                    iChessBoard[x, y] = 0;
+                }
+            }
+        }
+
+        public void nQueensBackTrack()
+        {
+            //Start backtrack
+            for (int position = 0; ; )
+            {
+                iPlaceHolder[position]++;
+                //Move a back a column
+                if (iPlaceHolder[position] == iQueens)
+                {
+                    //get position
+                    iChessBoard[iPlaceHolder[position] - 1, position] = 0;
+                    iPlaceHolder[position] = -1;
+                    position--;
+                    //if the position of solution is not on the board, there was no solutions found, prompt user and end
+                    if (position == -1)
+                    {
+                        if (iCount == 0)
+                        {
+                            tbSolutions.Text = "No Solutions Found.";
+                            rbGUIOutput.AppendText("No Solutions Found.");
+                        }
+                        tbStatus.Text = "Finished checking board.";
+
+                        break;
+                    }
+                }
+                else
+                {
+                    //get position
+                    iChessBoard[iPlaceHolder[position], position] = 1;
+                    if (iPlaceHolder[position] != 0)
+                    {
+                        iChessBoard[iPlaceHolder[position] - 1, position] = 0;
+                    }
+                    if (CheckAllPositions(iChessBoard, iQueens))
+                    {
+                        //increment to the next column to begin
+                        position++;
+                        if (position == iQueens)
+                        {
+                            position--;
+                            iCount++;
+
+                            //update how many solutions are found
+                            tbSolutions.Text = Convert.ToString(iCount);
+                            UpdateFields();
+
+
+                            //if there is a valid solution, print a 1 on the board, else print a 0; all of this is in the correct [x,y] coordinate
+                            rbGUIOutput.AppendText("Board" + iCount.ToString() + ":\n");
+                            for (int xCord = 0; xCord < iQueens; xCord++)
+                            {
+                                for (int yCord = 0; yCord < iQueens; yCord++)
+                                {
+                                    rbGUIOutput.AppendText(iChessBoard[xCord, yCord] + " ");
+                                }
+                                rbGUIOutput.AppendText("\n");
+                            }
+                            rbGUIOutput.AppendText("\n");
+                        }
+                    }
+                    //increment attempts to find a solution and dynamically show to user
+                    iAttempts++;
+                    tbAttempts.Text = Convert.ToString(iAttempts);
+                    UpdateFields();
+                }
+            }
+
+        }
+        public static bool CheckAllPositions(int[,] board, int N)
+        {
+            //Check the rows for a valid position
+            for (int l = 0; l < N; l++)
+            {
+                int iValidSolutionSum = 0;
+                for (int r = 0; r < N; r++)
+                {
+                    iValidSolutionSum = iValidSolutionSum + board[l, r];
+                }
+                if (iValidSolutionSum > 1)
+                {
+                    return false;
+                }
+            }
+            //Check the diagonals for a valid position
+            //check the diagonal above the current position
+            for (int l = 0, r = N - 2; r >= 0; r--)
+            {
+                int iValidSolutionSum = 0;
+                for (int xCord = l, yCord = r; yCord < N; xCord++, yCord++)
+                {
+                    iValidSolutionSum = iValidSolutionSum + board[xCord, yCord];
+                }
+                if (iValidSolutionSum > 1)
+                {
+                    return false;
+                }
+            }
+            //check the diagonal below current position
+            for (int l = 1, r = 0; l < N - 1; l++)
+            {
+                int iValidSolutionSum = 0;
+                for (int xCord = l, yCord = r; xCord < N; xCord++, yCord++)
+                {
+                    iValidSolutionSum = iValidSolutionSum + board[xCord, yCord];
+                }
+                if (iValidSolutionSum > 1)
+                {
+                    return false;
+                }
+            }
+            //Check the other diagonals
+            //check diagonal to the left
+            for (int l = 0, r = 1; r < N; r++)
+            {
+                int iValidSolutionSum = 0;
+                for (int xCord = l, yCord = r; yCord >= 0; xCord++, yCord--)
+                {
+                    iValidSolutionSum = iValidSolutionSum + board[xCord, yCord];
+                }
+                if (iValidSolutionSum > 1)
+                {
+                    return false;
+                }
+            }
+            //check diagonals below and to the left
+            for (int l = 1, r = N - 1; l < N - 1; l++)
+            {
+                int iValidSolutionSum = 0;
+                for (int xCord = l, yCord = r; xCord < N; xCord++, yCord--)
+                {
+                    iValidSolutionSum = iValidSolutionSum + board[xCord, yCord];
+                }
+                if (iValidSolutionSum > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        public void ResetVariables()
+        {
+            iQueens = 0;
+            iCount = 0;
+            iChessBoard = null;
+            iAttempts = 0;
+            iPlaceHolder = null;
+
+        }
+
+        public void ClearTextBoxes()
+        {
+            tbStatus.Text = "";
+            tbSolutions.Text = "";
+            tbAttempts.Text = "";
+            tbRunningTime.Text = "";
+            rbGUIOutput.Clear();
+
+        }
+
+        public void UpdateFields()
+        {
+            Application.DoEvents();
+        }
+
+
+    }
+}
